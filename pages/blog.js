24ce68a8790b590f23dcb19cd6a5d8fb3baf750c
Chia-Sheng Chen <mail@cschen.io>
2022-08-06 19:25:16 +0800

    (blogs): 2022-08-04 (#1)
    * Next.js blog with local Markdown files -- Part 1
    * Fetch blog post by filename and convert md to html

--- pages/blog.js
+++ pages/blog.js
@@ -1,21 +1,33 @@
 import React from "react";
 import Link from "next/link";
 import { Text, Container } from "@chakra-ui/react";
+import path from "path";
+import { promises as fs } from "fs";
 
 export default function Blog(props) {
+  const { posts } = props;
   return (
     <Container maxW="container.lg">
-          <Text fontSize="4xl" as="h2">
-            Should I write?
-          </Text>
-          <hr/>
-          <Text fontSize="2xl">
-            Should I start to write blogs?
-            I was never a content producer, though I&apos;d love to talk and to say stories that I&apos;ve heard of or personally experienced.
-            And I&apos;ve kept believing that writing everyday should result in lots of rewards in the long run. But could I write everyday?
-            What would be my writing style? What shall be the topics that I have passion to write about?
-            I have no idea, but let&apos;s see if I can concentrate and keep working on this long project.
-          </Text>
+      {posts.map(post =>
+        <div key={post}>
+          <Link href={`/blog/${post.replace(".md", "")}`} passHref>
+            <a>
+              {post}
+            </a>
+          </Link>
+        </div>,
+      )}
     </Container>
   );
 }
+
+export const getStaticProps = async (context) => {
+  const folder = path.join(process.cwd(), "blogs");
+  const posts = await fs.readdir(folder);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
